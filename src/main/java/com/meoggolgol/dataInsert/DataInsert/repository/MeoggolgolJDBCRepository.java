d95cb85e7ab67176e811a25ff6f5bd1c4e1986d8
weseonggu <fkskdldh@naver.com>
2023-05-16 16:13:40 +0900

    db에 데이터 저장하기

--- src/main/java/com/meoggolgol/dataInsert/DataInsert/repository/MeoggolgolJDBCRepository.java
+++ src/main/java/com/meoggolgol/dataInsert/DataInsert/repository/MeoggolgolJDBCRepository.java
@@ -1,12 +1,37 @@
 package com.meoggolgol.dataInsert.DataInsert.repository;
 
+import java.util.ArrayList;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.meoggolgol.dataInsert.DataInsert.convertCSV.Meoggolgol;
 
 import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
 public class MeoggolgolJDBCRepository {
+	
 	private final JdbcTemplate jdbc;
+	
+	private static String insert_MeoggolgolInfo=
+			"""
+			insert into Alley_information(FCLTY_NM, RDNMADR_NM, SIGNGU_CD, FCLTY_LO, FCLTY_LA, NEARBY_PBTRNSP_NM, PARKNG_POSBL_AT)
+			VALUES(?,?,?,?,?,?,?);
+			""";
+	
+	public void insert(ArrayList<Meoggolgol> list) {
+		for (Meoggolgol mgg : list) {
+			jdbc.update(insert_MeoggolgolInfo,
+					mgg.getFCLTY_NM(),
+					mgg.getRDNMADR_NM(),
+					mgg.getSIGNGU_CD(),
+					mgg.getFCLTY_LO(),
+					mgg.getFCLTY_LA(),
+					mgg.getNEARBY_PBTRNSP_NM(),
+					mgg.getPARKNG_POSBL_AT()
+					);
+		}
+	}
 }
--- src/main/java/com/meoggolgol/dataInsert/DataInsert/runner/JdbcCommandLineRunner.java
+++ src/main/java/com/meoggolgol/dataInsert/DataInsert/runner/JdbcCommandLineRunner.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 import com.meoggolgol.dataInsert.DataInsert.convertCSV.Meoggolgol;
 import com.meoggolgol.dataInsert.DataInsert.convertCSV.ReadCSVService;
-import com.meoggolgol.dataInsert.DataInsert.convertCSV.SiDoGun;
+import com.meoggolgol.dataInsert.DataInsert.repository.MeoggolgolJDBCRepository;
 
 import lombok.RequiredArgsConstructor;
 @Component
@@ -15,12 +15,15 @@ import lombok.RequiredArgsConstructor;
 public class JdbcCommandLineRunner implements CommandLineRunner {
 	
 	private final ReadCSVService rcs;
+	private final MeoggolgolJDBCRepository mjr;
 	@Override
 	public void run(String... args) throws Exception {
 		ArrayList<Meoggolgol> list = rcs.readMeoggolgolcsv();
-		for (Meoggolgol meoggolgol : list) {
-			System.out.println(meoggolgol);
-		}
+//		for (Meoggolgol meoggolgol : list) {
+//			System.out.println(meoggolgol.getSIGNGU_CD());
+//		}
+		
+		mjr.insert(list);
 
 	}
 }
