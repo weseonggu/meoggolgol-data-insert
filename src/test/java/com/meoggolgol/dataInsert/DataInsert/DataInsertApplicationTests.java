52ea8f894094e3d8283ac26ba8ed064d9882416b
weseonggu <fkskdldh@naver.com>
2023-05-16 10:43:44 +0900

    먹자골목 정보 입력 준비

---
+++ src/test/java/com/meoggolgol/dataInsert/DataInsert/DataInsertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meoggolgol.dataInsert.DataInsert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataInsertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
